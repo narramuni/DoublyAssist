dba85a6c16f9f9b180d541fed27705d35bf2632c
narramuni <nmspavan@gmail.com>
2023-10-17 14:10:53 +0530

    Add project files.

---
+++ DoublyLinkedListClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DoublyLinkedList
+{
+    public class DoubleLinkedList
+    {
+        private Node root = null;
+        private Node current = null;
+        private int length;
+
+        public Node Root
+        {
+            get { return this.root; }
+            set { this.root = value; }
+        }
+
+        public int Length
+        {
+            get { return this.length; }
+            set { this.length = value; }
+        }
+
+        public void Add(Node node)
+        {
+            node.Next = null;
+            if (root == null)
+            {
+                root = node;
+                length = 1;
+                current = root;
+                root.Prev = null;
+            }
+            else
+            {
+                current.Next = node;
+                node.Prev = current;
+                current = node;
+                length++;
+            }
+        }
+
+        public Node GetCurrent()
+        {
+            return this.current;
+        }
+
+        public Node GetRoot()
+        {
+            current = root;
+            return this.root;
+        }
+
+        public Node GetNext()
+        {
+            if (current.Next != null)
+            {
+                current = current.Next;
+                return current;
+            }
+            else
+                return null;
+        }
+
+        public Node GetPrev()
+        {
+            if (current.Prev != null)
+            {
+                current = current.Prev;
+                return current;
+            }
+            else
+                return null;
+        }
+
+        public int GetLengthOfList()
+        {
+            return this.length;
+        }
+
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            DoubleLinkedList doublyLinkedList = new DoubleLinkedList();
+
+            // Adding nodes with predefined values
+            Node node1 = new Node { Data = "Array" };
+            Node node2 = new Node { Data = "Stack" };
+            Node node3 = new Node { Data = "Queue" };
+
+            doublyLinkedList.Add(node1);
+            doublyLinkedList.Add(node2);
+            doublyLinkedList.Add(node3);
+
+            Console.WriteLine("Forward Traversal:");
+            Node current = doublyLinkedList.GetRoot();
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = doublyLinkedList.GetNext();
+            }
+
+            Console.WriteLine("Backward Traversal:");
+            current = doublyLinkedList.GetCurrent();
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = doublyLinkedList.GetPrev();
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+ }
